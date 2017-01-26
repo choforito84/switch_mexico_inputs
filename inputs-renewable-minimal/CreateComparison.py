c203277d6060c2014639dfc1ac0914d2c5de1526
choforito84 <sayeg_84@hotmail.com>
2017-01-26 14:52:36 -0800

    updated repository

---
+++ inputs-renewable-minimal/CreateComparison.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+df1=pd.read_csv('load_zones.tab',sep="\t",index_col=0)
+df2=pd.read_csv('loads.tab',sep="\t",index_col=0)
+df3=pd.read_csv('project_info.tab',sep='\t')
+df4=pd.DataFrame(index=df1.index,columns=['distribution','generation','peak_load'])
+for i in df4.index:
+    df4.loc[i,'distribution']=float(df1.loc[i,'existing_local_td'])
+    df4.loc[i,'generation']=df3[df3['proj_load_zone']==i]['proj_capacity_limit_mw'].sum()
+    df4.loc[i,'peak_load']=df2.loc[i,'lz_demand_mw'].max()
+for name in df4.columns:
+    df4.loc['total',name]=df4[name].sum()
+df4['accomplish']=(df4['generation']>=df4['peak_load'])
+df4.to_csv('comparison.csv')
